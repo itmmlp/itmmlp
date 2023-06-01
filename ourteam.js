8f7775da34e48ebf3b624d38c6357a3af6441302
IT Mission Malappuram <49332664+itmmlp@users.noreply.github.com>
2023-06-01 12:45:01 +0530

    Create ourteam.js

---
+++ ourteam.js
@@ -0,0 +1,83 @@
+var swiper = new Swiper(".team-1-swiper", {
+  navigation: {
+    nextEl: ".swiper-button-nexts",
+    prevEl: ".swiper-button-prevs",
+  },
+  loop: true,
+  breakpoints: {
+
+    100: {
+      slidesPerView: 1.2,
+      spaceBetween: 40
+    },
+
+    340: {
+      slidesPerView: 1.5,
+      spaceBetween: 40
+    },
+
+    500: {
+      slidesPerView: 1.5,
+      spaceBetween: 20
+    },
+
+    630: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+
+    769: {
+      slidesPerView: 1.5,
+      spaceBetween: 30
+    },
+
+    890: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    1090: {
+      slidesPerView: 2.5,
+      spaceBetween: 40
+    },
+  }
+});
+
+
+var swiper2 = new Swiper(".team-2-swiper", {
+  navigation: {
+    nextEl: ".swiper-button-nexts2",
+    prevEl: ".swiper-button-prevs2",
+  },
+  loop: true,
+  breakpoints: {
+
+    300: {
+      slidesPerView: 1.5,
+      spaceBetween: 40
+    },
+
+    500: {
+      slidesPerView: 1.5,
+      spaceBetween: 20
+    },
+
+    630: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+
+    769: {
+      slidesPerView: 1.5,
+      spaceBetween: 30
+    },
+
+    890: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    1090: {
+      slidesPerView: 2.5,
+      spaceBetween: 40
+    },
+  }
+});
